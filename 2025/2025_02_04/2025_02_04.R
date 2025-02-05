088155eba00a557dd464b0799abcc76f877003f2
MWypych <wypych.mic@gmail.com>
2025-02-05 10:34:11 +0100

    final fixes
    -use geom text repel
    -ditch table
    -fix colors and backgrounds

--- 2025/2025_02_04/2025_02_04.R
+++ 2025/2025_02_04/2025_02_04.R
@@ -1,13 +1,11 @@
 library(tidyverse)
 library(showtext)
-
+library(ggrepel)
 font_add_google("Lobster")
 showtext_opts(dpi = 300)
 showtext_auto(enable = T)
 
-simpsons_characters <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-02-04/simpsons_characters.csv')
-simpsons_episodes <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-02-04/simpsons_episodes.csv')
-simpsons_locations <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-02-04/simpsons_locations.csv')
+
 simpsons_script_lines <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-02-04/simpsons_script_lines.csv')
 
 
@@ -29,10 +27,10 @@ donut_plot <- simpsons_spoken %>%
          ymax = cumsum(percentage),
          ymin = c(0, head(ymax, n=-1)),
          label_pos = (ymax + ymin) / 2,
-         label = paste0(raw_character_text, " : ", round(percentage, 2), "%")) %>%
+         label = paste0(raw_character_text, " : ", round(percentage*100, 2), "%")) %>%
   ggplot(aes(ymax=ymax, ymin=ymin, xmax=4, xmin=3, fill=raw_character_text)) +
   geom_rect() +
-  geom_textbox(aes( x=3.5, y=label_pos, label=label), size = 1, family = "Lobster", inherit.aes = FALSE) +
+  geom_text_repel(aes( x=3.5, y=label_pos, label=label), color = "white", size = 5, family = "Lobster", inherit.aes = FALSE) +
   scale_fill_manual(values = colors) +
   coord_polar(theta="y") + 
   xlim(c(0, 4)) + 
@@ -41,20 +39,11 @@ donut_plot <- simpsons_spoken %>%
   theme(legend.position = "none",
         text = element_text(family = "Lobster"),
         plot.title.position = "plot",
-        plot.background = element_rect(fill = "grey80", color = "grey80"))
-  
-#to do: get the background to work
-# get the labels to work?
+        plot.background = element_rect(fill = "pink3", color = "pink3"),
+        panel.background = element_rect(fill = "pink3", color = "pink3"),
+        plot.title = element_text(color = "white", size = 25)) 
 
 
-# add a table with percentages?
 
-perc_table <- simpsons_spoken %>%
-  mutate(simpson = str_detect(raw_character_text, "Simpson")) %>%
-  filter(simpson == TRUE) %>%
-  group_by(raw_character_text) %>%
-  summarize(sum_words = sum(word_count, na.rm = T)) %>%
-  mutate(percentage = paste0(round(sum_words/sum(sum_words)*100, 2), "%")) %>%
-  gt::gt()
+ggsave("simpsons.png", donut_plot, width = 6, height = 6)
 
-grid.arrange(donut_plot,tableGrob(perc_table)) 
