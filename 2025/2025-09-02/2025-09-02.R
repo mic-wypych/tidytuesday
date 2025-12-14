e9d0c4fd93916e63040f469fbed69372af6b74a8
MWypych <wypych.mic@gmail.com>
2025-12-14 19:50:48 +0100

    upadte

--- 2025/2025-09-02/2025-09-02.R
+++ 2025/2025-09-02/2025-09-02.R
@@ -21,7 +21,6 @@ top_recorders <- frogID_data %>%
 
 
 lag_data <- frogID_data %>%
- # filter(recordedBy %in% top_recorders) %>%
   inner_join(frog_names, by = "scientificName") %>%
   group_by(tribe, recordedBy) %>%
   mutate(event_datetime = ymd_hms(paste(eventDate, eventTime))) %>%
@@ -33,7 +32,9 @@ lag_data <- frogID_data %>%
   mutate(row = 1,
          y = cumsum(row), .by = c("tribe", "recordedBy"))
 
-
+lag_data$tribe <- factor(lag_data$tribe, ordered = T,
+                         levels = c("Limnodynastidae", "Myobatrachidae", "Pelodryadidae",
+                                    "Asterophryninae", "Bufonidae", "Ranidae"))
 
 #titles and stuff
 
@@ -41,7 +42,7 @@ title <- "Australia's most active frog spotters"
 
 subtitle <- str_wrap("Citizen scientists across Australia contribute frog call recordings to FrogID,
 a nationwide project tracking the continent’s unique and threatened frog fauna. This plot
-highlights frog tribes and the five most prolific recorders (IDs: 35320, 5742, 35717, 12664, 41581),
+highlights frog tribes and the five most prolific recorders in color (IDs: 35320, 5742, 35717, 12664, 41581),
 with each ‘hop’ representing the time gap between consecutive recordings. 
                      Y axis shows cumulative number of frogs from a given tribe spotted", 100)
 caption <- "Tidytuesday 2025-09-02 | data from FrogID data | Michał Wypych"
@@ -55,7 +56,7 @@ test_1.1 <-
     curvature = -0.01,
     ncp = 5,         
     alpha = 0.1,
-    linewidth = 1
+    linewidth = .1
   ) +
   geom_curve(
     data = lag_data %>% drop_na() %>%filter(recordedBy %in% top_recorders) %>% arrange(desc(event_datetime)),
@@ -65,7 +66,7 @@ test_1.1 <-
     alpha = 0.8,
     linewidth = 1
   ) +
-  geom_text(aes(x = as_datetime("2023-02-01 00:00:01"), y = 600, label = tribe),
+  geom_text(aes(x = as_datetime("2023-02-01 00:00:01"), y = 650, label = tribe),
             family = font, size = 9, inherit.aes= FALSE) +
   scale_x_datetime(date_breaks = "1 month", date_labels = "%B") +
   scale_color_manual(values = c("#3c1642", "#086375", "#1dd3b0","#16db65", "#affc41")) +
